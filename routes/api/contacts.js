4cdfef8b6bce2eb332f71d13795daaa64bb1786d
MartaAnetaSzymanska <szymanska.ma@gmail.com>
2024-09-21 21:44:22 +0200

    first_get

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,30 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
+const Contact = require("../../models/contact");
 
-const router = express.Router()
+router.get("/", async (req, res, next) => {
+  const contacts = await Contact.find({});
+  res.json(contacts);
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.patch("/:contactId/favorite", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,32 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+require("dotenv").config();
+
+const urlDb = process.env.DB_HOST;
+
+const connection = mongoose.connect(urlDb);
+
+// connection
+//   .then(() => {
+//     console.log("DB connected");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     process.exit(1); - kończy proces nieudanego połączenia
+//   });
+
+const startServer = async () => {
+  try {
+    await connection;
+    console.log("DB connected");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+startServer();
