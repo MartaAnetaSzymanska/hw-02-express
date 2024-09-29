8db18e6040956c604567f1341facdd3dca69ca90
MartaAnetaSzymanska <szymanska.ma@gmail.com>
2024-09-29 19:16:34 +0200

    revert

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,85 @@
 const express = require("express");
-
 const router = express.Router();
 
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+  updateStatus,
+} = require("../../service/services");
+
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const contacts = await listContacts();
+    res.status(200).json(contacts);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const contact = await getContactById(req.params.contactId);
+    if (!contact) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json(contact);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const newContact = await addContact(req.body);
+    res.status(201).json(newContact);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const contact = await removeContact(req.params.contactId);
+    if (!contact) {
+      return res.status(404).json({ message: "Not Found" });
+    }
+    res.status(200).json({ message: "Contact deleted" });
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const updatedContact = await updateContact(contactId, req.body);
+    if (!updatedContact) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json(updatedContact);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.patch("/:contactId/favorite", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const { favorite } = req.body;
+    if (favorite === undefined) {
+      return res.status(400).json({ message: "Missing field favorite" });
+    }
+    const updatedStatus = await updateStatus(contactId, { favorite });
+    if (!updatedStatus) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json(updatedStatus);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
